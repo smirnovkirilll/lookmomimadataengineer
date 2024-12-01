0d3e7d4757969dd1ba64bdc606b8ff6bde4bd777
kirill <kirillturkey7497@gmail.com>
2024-12-01 15:35:54 +0300

    debugged most of resources creation

--- reddit_digest/terraform/main.tf
+++ reddit_digest/terraform/main.tf
@@ -1,5 +1,8 @@
 #!/bin/env -S terraform plan
 # https://developer.hashicorp.com/terraform/tutorials/configuration-language/sensitive-variables
+# terraform init
+# terraform plan -var-file="secret.tfvars"
+# terraform apply -var-file="secret.tfvars"
 
 
 terraform {
@@ -27,37 +30,41 @@ resource "yandex_storage_bucket" "bucket" {
 }
 
 
-# todo: doesn't work properly, created support ticket
 resource "yandex_lockbox_secret" "reddit_digest" {
   name = "reddit_digest"
+  description = "sensitive information for reddit-digest application"
 }
 
 
-# todo: doesn't work properly, created support ticket
 resource "yandex_lockbox_secret_version_hashed" "version_0" {
   secret_id = yandex_lockbox_secret.reddit_digest.id
   key_1 = "ACCESS_KEY_ID"
   text_value_1 = var.s3_access_key
   key_2 = "SECRET_ACCESS_KEY"
   text_value_2 = var.s3_secret_key
-  description = "sensitive information for "
 }
 
 
+resource "random_uuid" "new_on_every_run" {}
+
+
 resource "null_resource" "reddit_digest_zip" {
+  triggers = {
+    uuid = random_uuid.new_on_every_run.id
+  }
   provisioner "local-exec" {
-    command = "zip reddit_digest ../requirements.txt ../helpers.py ../reddit_request.py ../main.py"
+    command = "mkdir app; cp ../{requirements.txt,__init__.py,helpers.py,reddit_request.py,main.py} app; zip -r reddit_digest app; rm -rf app;"
   }
 }
 
 
-#  todo: doesn't work properly, created support ticket
+# todo: does not work properly yet
 resource "yandex_function" "reddit_digest_ingest_raw" {
-  name               = "reddit_digest_ingest_raw"
+  name               = "reddit-digest-ingest-raw"
   description        = "requests some subreddits on reddit, saves response to s3"
-  user_hash          = "v0"
-  runtime            = "python313"
-  entrypoint         = "main.handler"
+  user_hash          = random_uuid.new_on_every_run.id
+  runtime            = "python312"
+  entrypoint         = "app.main.handler"
   memory             = "128"
   execution_timeout  = "10"
   service_account_id = var.service_account_id
@@ -76,14 +83,22 @@ resource "yandex_function" "reddit_digest_ingest_raw" {
 }
 
 
-#  todo: doesn't work properly, created support ticket
 resource "yandex_function_trigger" "reddit_digest_ingest_raw_trigger" {
-  name        = "reddit_digest_ingest_raw_trigger"
-  description = "starts reddit_digest_ingest_raw"
+  name        = "reddit-digest-ingest-raw-trigger"
+  description = "starts function reddit-digest-ingest-raw"
   timer {
-    cron_expression = "0 10 * * *"
+    # crontab-guru says it's incorrect, but YC wants this format
+    cron_expression = "0 10 * * ? *"
   }
   function {
     id = yandex_function.reddit_digest_ingest_raw.id
+    service_account_id = var.service_account_id
   }
 }
+
+
+output yandex_storage_bucket_id {value = yandex_storage_bucket.bucket.id}
+output yandex_lockbox_secret_id {value = yandex_lockbox_secret.reddit_digest.id}
+output reddit_digest_ingest_raw_id {value = yandex_function.reddit_digest_ingest_raw.id}
+output reddit_digest_ingest_raw_user_hash {value = random_uuid.new_on_every_run.id}
+output yandex_function_trigger_id {value = yandex_function_trigger.reddit_digest_ingest_raw_trigger.id}
