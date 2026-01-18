7a33feb751fbb55681f7937c41605c4a61b538bb
kirill <kirillturkey7497@gmail.com>
2026-01-18 12:54:08 +0300

    minor style changes

--- website/pages/curated_list.js
+++ website/pages/curated_list.js
@@ -1,12 +1,27 @@
 let gridApiCL;
 const gridOptionsCL = {
   rowData: [],
-  autoSizeStrategy: {
-    type: "fitCellContents"
-  },
+  getRowId: params => params.data.title,
+  autoSizeStrategy: {type: "fitCellContents"},
   tooltipShowMode: "whenTruncated",
   tooltipShowDelay: 1000,
+  pagination: true,
+  enableCellTextSelection: true,
+  defaultColDef: {filter: true},
+  rowClassRules: {
+    "row-background-grey": "data.background_color == 'GREY'",
+    "row-background-pink": "data.background_color == 'PINK'",
+    "row-background-red": "data.background_color == 'RED'",
+  },
+
   columnDefs: [
+    {
+      headerName: "#",
+      valueGetter: params => params.node.rowIndex + 1,
+      maxWidth: 75,
+      sortable: false,
+      filter: false,
+    },
     {
       field: "title",
       cellRenderer: TitleCellLinkRendererCL,
@@ -29,10 +44,6 @@ const gridOptionsCL = {
       tooltipField: "comment",
     },
   ],
-  pagination: true,
-  defaultColDef: {
-    filter: true
-  }
 };
 
 
@@ -55,6 +66,16 @@ function TitleCellLinkRendererCL(params) {
   </a>`;
   return link;
 }
+
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === '/') {
+    var filter = document.getElementById('curated-list-filter');
+    filter.setSelectionRange(0, filter.value.length);
+    filter.focus();
+    event.preventDefault();
+  }
+});
 
 
 gridApiCL = agGrid.createGrid(document.querySelector("#CuratedListGrid"), gridOptionsCL);
